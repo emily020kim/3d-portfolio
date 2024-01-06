5f235f81297e7071d41c0017af92dd09343d99d8
emily020kim <emily020kim@gmail.com>
2024-01-05 20:38:49 -0800

    Edit to link

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -45,7 +45,7 @@ const Projects = () => {
                   rel='noopener noreferrer'
                   className='font-semibold text-blue-600'
                 >
-                  Live Link
+                  Source Code
                 </Link>
               </div>
             </div>
